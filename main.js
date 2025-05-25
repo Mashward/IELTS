87ae19a8d526e5f8bf72b261b3953e6b4097f4ab
Mashward <136252181+Mashward@users.noreply.github.com>
2025-05-25 15:17:39 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("root").insertAdjacentHTML("beforeend", "<p>[JS Loaded] React app script would go here.</p>");
+});
